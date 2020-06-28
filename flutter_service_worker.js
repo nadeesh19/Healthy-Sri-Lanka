2be347da3363e2dff19c65a3f502c365c98feef7
Nadeesha Maduman <>
2020-06-28 13:12:26 +0530

    added 2020/06/(22,23,24,25,26,27) SL covid details

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "540c12d71db9f395ba69715fbb71bf36",
 "/": "540c12d71db9f395ba69715fbb71bf36",
-"main.dart.js": "2a37aa5fdc4c1134328673450fc4dd16",
+"main.dart.js": "c5d585b745e59e61d8f19fbe7d154bfb",
 "assets/LICENSE": "dbaf3fadb830d341d59b1b35d3922225",
 "assets/images/ic_patient.png": "10847876bf497fc195a0327d41899735",
 "assets/images/ic_active.png": "db1f42508e1da6798d021f887bfae8cc",
